aefea164a7d967a53f62482b7c8b7ea99f6fc94d
Alex10101 <cpt-morgan@mail.ru>
2019-03-14 20:52:26 +0300

    dump after react-table

--- views/src/components/TableComponent.js
+++ views/src/components/TableComponent.js
@@ -1,6 +1,5 @@
 import { connect } from 'react-redux';
 import * as actions from '../actions/articles';
-import Table from 'react-bootstrap/Table';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
@@ -11,10 +10,6 @@ import PropTypes from "prop-types";
 
 
 class TableComponent extends React.Component {
-
-	state = {
-		page: 1
-	}
 
 	componentWillMount() {
 		this.props.getArticles();
@@ -28,25 +23,53 @@ class TableComponent extends React.Component {
 		this.props.Show(article, 'popupArticle')
 	}
 
-	mapArticles() {
-
+	render() {
+		console.log('TableComponent', this.props)
+		return(
+			<div className="articleList">
+				{ this.props.articles.articles ? <Table data={this.props.articles.articles}/> : ''}
+			</div>
+		)
 	}
 
-	stashArticles() {
+}
 
-	}
+class Table extends React.Component {
 
-	renderArticles() {
-		let props = this.props.articles
-		if(props.articles) {
-			return props.articles.map( (article, index) => {
-				return [
-				    <tr key={'tr' + article._id}>
-			      	  <td key={'id' + article._id}> {article._id} </td>
-				      <td key={'title' + article._id}> {article.title} </td>
-				      <td key={'body' + article._id}> {article.body} </td>
-				      <td key={'buttons' + article._id}> 
-				      	<Link to={{ pathname: `/articles/${article._id}/edit`, article}}>
+	render() {
+		let article = ''
+		return(  
+			<ReactTable
+			    PaginationComponent={Pagination}
+			    defaultPageSize={4}
+			    className="-striped -highlight"
+			    data={this.props.data}
+			    columns={[
+			      {
+			        Header: "Id",
+			        accessor: "_id",
+			        sortable: false,
+			        resizable:false
+			      },
+			      {
+			        Header: "Title",
+			        accessor: "title",
+			        sortable: false,
+			        resizable:false
+			      },
+			      {
+			        Header: "Body",
+			        accessor: "body",
+			        sortable: false,
+			        resizable:false
+			      },
+			      {
+			        Header: " ",
+			        sortable: false,
+			        resizable:false,
+			        Cell: row => (
+			       		<div className="">
+			       		<Link to={{ pathname: `/articles/${article._id}/edit`, article}}>
 					      	<button 
 					      		className='btn btn-light table-row-button' 
 					      		onClick={() => this.handleEdit(article)} 
@@ -56,34 +79,14 @@ class TableComponent extends React.Component {
 				      		className='btn btn-light table-row-button' 
 				      		onClick={() => this.handleView(article)} 
 				      	>View</button> 
-				      </td>
-			        </tr>
-				]
-			})
-		}
-	}
 
-	render() {
-		console.log('TableComponent', this.props)
-		return(
-			<div className="articleList">
-				<Table striped bordered hover>
-				  <thead>
-				    <tr>
-				      <th>Id</th>
-				      <th>Title</th>
-				      <th>Body</th>
-				      <th></th>
-				    </tr>
-				  </thead>	
-				  <tbody>			  
-					{this.renderArticles()}
-				  </tbody>
-				</Table>
-			</div>
+			       		</div>
+			        )
+			      },
+			    ]}
+			/>
 		)
 	}
-
 }
 
 function mapStateToProps({ articles }) {
